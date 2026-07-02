7653039326101956751110275987581d75bab653
Jayesh Choudhary <choudharyjayesh668@gmail.com>
2026-07-02 22:57:09 +0530

    Server setup

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const express = require("express");
+const path = require("path");
+const Chat = require("./models/chat");
 const app = express();
-
+const PORT = 8080;
+const methodOverride=require("method-override");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 const mongoose = require("mongoose");
-const MONGO_URL="mongodb://127.0.0.1:27017/airbnb"
-main().then(()=>{
-    console.log("Data Base Connected")
-}).catch((error)=>{
-    console.log("Error in Data Base")
-});
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect("mongodb://127.0.0.1:27017/airbnb");
 }
-console.log("App started");
-
+main()
+    .then(() => {
+        console.log("MongoDB connected");
+        app.listen(PORT, () => {
+            console.log(`Server running on http://localhost:${PORT}`);
+        });
+    })
+    .catch(err => console.log(err));
 app.get("/", (req, res) => {
-    res.send("Hello World");
-});
-
-app.listen(8080, () => {
-    console.log("Server is Listening on port 8080");
+    res.send("Root is working");
 });
